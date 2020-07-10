7491f96f7c3b64d1a66fd552eaafe1aa4dfcea8c
przemyslaw.drozniak <biuro@interakcjo.pl>
2020-07-10 12:55:50 +0200

    v1.9.4 - Aria Hidden on SVG not on Button

--- sputnik-search.php
+++ sputnik-search.php
@@ -7,7 +7,7 @@
 Plugin Name: Sputnik Search
 Plugin URI: https://sputnik.pl
 Description: Advanced search in website using ElasticSearch
-Version: 1.9.3
+Version: 1.9.4
 Author: Sputnik
 Author URI: https://sputnik.pl
 Text Domain: sputnik-search
